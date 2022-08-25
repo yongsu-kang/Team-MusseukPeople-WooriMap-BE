b70b6b65e2c610b12b58a61cf2dd19e91fc87a5e
Yongsu Kang <dksktm15@naver.com>
2022-08-25 15:48:37 +0900

    [WOOR-341] feat: 커플 Repo soft delete 기능 구현

--- woorimap-core/src/main/java/com/musseukpeople/woorimap/couple/domain/CoupleRepository.java
+++ woorimap-core/src/main/java/com/musseukpeople/woorimap/couple/domain/CoupleRepository.java
@@ -1,8 +1,16 @@
 package com.musseukpeople.woorimap.couple.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.musseukpeople.woorimap.couple.infrastructure.CoupleQueryRepository;
 
 public interface CoupleRepository extends JpaRepository<Couple, Long>, CoupleQueryRepository {
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Couple c SET c.deleted = true WHERE c.id = :coupleId")
+    void updateDeletedById(@Param("coupleId") Long coupleId);
+
 }
